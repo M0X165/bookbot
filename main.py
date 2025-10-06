88942d39096c1b891f825e18c4e87b6f23db1cf2
M0X165 <simeonovmaxim1@gmail.com>
2025-10-06 21:52:12 +0300

    stats.py

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
+from stats import separation_of_words, number_of_words
+
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
-
-def separation_of_words(text):
-    return text.split()
-
-def number_of_words(words):
-    return len(words)
 
 def main():
     text = get_book_text("books/frankenstein.txt")
---
+++ stats.py
@@ -0,0 +1,7 @@
+def separation_of_words(text):
+    return text.split()
+
+def number_of_words(words):
+    return len(words)
+
+
