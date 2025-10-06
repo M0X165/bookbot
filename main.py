bb0090917406b0319d2ecf1eba3f0a02b57723df
M0X165 <simeonovmaxim1@gmail.com>
2025-10-06 22:42:16 +0300

    a lot for sorting the dictionary

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import separation_of_words, number_of_words
+from stats import separation_of_words, number_of_words, number_of_characters, sort_on, character_list
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -8,7 +8,13 @@ def main():
     text = get_book_text("books/frankenstein.txt")
     words = separation_of_words(text)
     num_words = number_of_words(words)
+    count = number_of_characters(text)
+    pairs = character_list(count)
     print(f"Found {num_words} total words")
+    for item in pairs:
+        ch = item["cha"]
+        if ch.isalpha():
+            print(f"{ch}: {item['num']}")
 
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -4,4 +4,20 @@ def separation_of_words(text):
 def number_of_words(words):
     return len(words)
 
+def number_of_characters(text):
+    count = {}
+    for character in text:
+        lowered = character.lower()
+        if lowered in count:
+            count[lowered] += 1
+        else:
+            count[lowered] = 1
+    return count
+        
+def sort_on(item):
+    return item["num"]
 
+def character_list(count):
+    pairs = [{"cha": ch, "num": n} for ch,n in count.items()]
+    pairs.sort(reverse=True, key=sort_on)
+    return pairs
